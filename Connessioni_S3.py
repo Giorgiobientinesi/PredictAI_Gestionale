9914b52d76391def469e83c92bf680219d5c7bf3
Giorgio Bientinesi <giorgiobientinesi@MBP-di-Giorgio.homenet.telecomitalia.it>
2024-12-23 18:23:56 +0100

    test secrets streamlit 2

--- Connessioni_S3.py
+++ Connessioni_S3.py
@@ -13,8 +13,8 @@ st.secrets["AWS_REGION"]
 def initialize_s3():
     s3 = boto3.client(
         's3',
-        st.secrets["AWS_ACCESS_KEY_ID"]
-        st.secrets["AWS_SECRET_ACCESS_KEY"]
+        st.secrets["AWS_ACCESS_KEY_ID"],
+        st.secrets["AWS_SECRET_ACCESS_KEY"],
         st.secrets["AWS_REGION"]
     )
     return s3
